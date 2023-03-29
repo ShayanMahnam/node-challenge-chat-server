7e05899ebb00185e49c780ef90d8e68dd1694183
Shayan Mahnam <shayan.mahnam.uk@gmail.com>
2023-03-29 16:02:02 +0100

    done

--- server.js
+++ server.js
@@ -6,27 +6,29 @@ const app = express();
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
 
 const welcomeMessage = {
   id: 0,
-  from: "Bart",
+  from: "Shayan",
   text: "Welcome to CYF chat system!",
 };
 
 //This array is our "data store".
 //We will start with one message in the array.
 //Note: messages will be lost when Glitch restarts our server.
-let messages = [welcomeMessage]
-
+let messages = [welcomeMessage];
 
 // level3 [5] read only text whose text contains a given substring
 // teniolao-cyf-chat-server.glitch.me/messages/search?text=express
-app.get("/messages/search", (req, res)=> {
-  const { term } = req.query
-  console.log(term)
-  
-  const filterMessages = messages.filter(message => message.text.toLowerCase().includes(term.toLowerCase()))
-  console.log(filterMessages)
+app.get("/messages/search", (req, res) => {
+  const { term } = req.query;
+  console.log(term);
+
+  const filterMessages = messages.filter((message) =>
+    message.text.toLowerCase().includes(term.toLowerCase())
+  );
+  console.log(filterMessages);
   res.send(filterMessages);
 });
 
@@ -38,34 +40,30 @@ app.get("/messages", (req, res) => {
 //read only the most recent 10 messages
 app.get("/messages/latest", (req, res) => {
   //for(let i=)
- const filterMessages = messages.filter((message, index) => messages.length-10 <= index)
- res.send(filterMessages)
+  const filterMessages = messages.filter(
+    (message, index) => messages.length - 10 <= index
+  );
+  res.send(filterMessages);
 });
 
 //[3] read one message specified by id
 app.get("/messages/:id", function (req, res) {
   const id = req.params.id;
-  messages = messages.filter(
-    (message) => message.id === Number(id)
-  );
-    res.status(200).send(messages);
+  messages = messages.filter((message) => message.id === Number(id));
+  res.status(200).send(messages);
 });
 
 // [1] create a new message
 app.post("/messages", (req, res) => {
   const { from, text } = req.body;
-  // console.log(req.body)
   const ourMessageObject = {
-    id: messages.length, 
+    id: Date.now(), //use date was better unique id instead of array length
     from,
     text,
-    timeSent: new Date().toLocaleDateString()
-  }
-  if(from.length === 0 || text.length === 0){
-    return res.status(400).send("please complete body")
-  } else {
-     messages.push(ourMessageObject);
-  }
+    timeSent: new Date().toLocaleDateString(),
+  };
+  messages.push(ourMessageObject);
+  res.send("Message received successfully.");
 });
 
 //[4] delete a message by id
@@ -75,9 +73,31 @@ app.delete("/messages/:id", (req, res) => {
   res.json(messages);
 });
 
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/index.html");
+// [6] update a message by id
+app.put("/messages/:id", (req, res) => {
+  const id = req.params.id;
+  const { text } = req.body;
+
+  let updatedMessage;
+  messages = messages.map((message) => {
+    if (message.id === Number(id)) {
+      updatedMessage = { ...message, text };
+      return updatedMessage;
+    }
+    return message;
+  });
+
+  if (updatedMessage) {
+    res.status(200).send(updatedMessage);
+  } else {
+    res.status(404).send("Message not found");
+  }
 });
-app.listen(process.env.PORT || 3000, () => {
-  console.log("server running!");
+
+// app.get("/", function (req, res) {
+//   res.sendFile(__dirname + "/index.html");
+// });
+
+const listener = app.listen(process.env.PORT, function () {
+  console.log("Your app is listening on port " + listener.address().port);
 });
